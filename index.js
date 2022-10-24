231a66ef52620acebc3c3b43b0b5f66e7ac68dd8
rhigladney <71747665+rhigladney@users.noreply.github.com>
2022-10-24 11:52:11 -0700

    1 file modified

--- index.js
+++ index.js
@@ -7,10 +7,26 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 background(148, 451, 255); // RGB
 
+var eyeSize = 33; // var eyeSize; eyeSize = 20;
+
+var x = 200;
+
+noStroke();
+
 // face
-ellipse(202, 208, 300);
-ellipse(304, 142, 40 40);
+fill(255, 255, 0);
+ellipse(202, 208, 300, 300);
+
+// eyes
+fill(255, 255, 0);
+ellipse(x - 50, 208, 300);
+ellipse(x + 100, 142, 40, 40);
 
 //mouth 
-ellipse(257, 240, 120, 136);
+fill(255, 255, 0);
+ellipse(x + 50, 240, 120, 136);
 
+// headband
+strokeWeight(77);
+Stroke(112,0,242);
+line(80, 116, 271, 70);
